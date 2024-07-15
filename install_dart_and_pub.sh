11a122d0c8389edff7f5d37b043655ec043269c2
Chayapol Mahatthanachai <best37@hotmail.com>
2024-07-15 23:15:16 +0700

    Fixed Dart installation

--- install_dart_and_pub.sh
+++ install_dart_and_pub.sh
@@ -1,29 +1,5 @@
 #!/bin/bash
-
-# Set Dart SDK URL
 DART_SDK_URL="https://storage.googleapis.com/dart-archive/channels/stable/release/latest/sdk/dartsdk-linux-x64-release.zip"
-
-# Create a temporary directory for the Dart SDK
-mkdir -p /app/dart-sdk
-
-# Download Dart SDK
-curl -o /tmp/dartsdk.zip $DART_SDK_URL
-
-# Unzip Dart SDK
-unzip /tmp/dartsdk.zip -d /app/dart-sdk
-
-# Add Dart SDK to PATH
-export PATH="/app/dart-sdk/dart-sdk/bin:$PATH"
-
-# Verify Dart SDK installation
-dart --version
-
-# Run dart pub get
-if command -v dart &> /dev/null
-then
-    echo "Running dart pub get"
-    l pub get
-else
-    echo "ERROR: dart could not be found."
-    exit 1
-fi
+curl -O $DART_SDK_URL
+unzip dartsdk-linux-x64-release.zip -d /app/
+export PATH=$PATH:/app/dart-sdk/bin
